54d1e1fdbd85e6e0652f25099e48d6865fd6fef6
Jeff <4a.45.46.46@gmail.com>
2022-08-13 11:10:57 -0700

    2-1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    
     [SerializeField] private float horizontal;
     [SerializeField] private float vertical;
-    [SerializeField] private float moveSpeed;
+    // player's current horizontal and vertical input
     [SerializeField] private float movementMultiplier = 0.1f;
+    // multiplier to prevent player from flying off the screen
     private float moveLimiter = 1.414f;
+    // sqrt2 to counteract diagonal movement (pythagorean)
 
     private void Start() {
 
     }
 
     private void Update() {
+        // input should be read in update, every frame
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        // make sure to capitalize and use GetAxisRaw
     }
 
     private void FixedUpdate() {
@@ -24,9 +27,11 @@ public class Player : MonoBehaviour {
             horizontal /= moveLimiter;
             vertical /= moveLimiter;
         }
+        // limit player's movement if they are going diagonally
         transform.position = new Vector2(
             transform.position.x + horizontal * movementMultiplier, 
             transform.position.y + vertical * movementMultiplier
         );
+        // update the player's position
     }
 }
